e03d5671259ca5bcf0c29ce9ecc031bba04f7f89
Gabriel Bretschner <software@kanedo.net>
2014-09-30 14:29:26 +0200

    added PHPDoc documentation

--- wp-responsive-images.php
+++ wp-responsive-images.php
@@ -36,7 +36,12 @@ if ( !class_exists( "Kanedo_WP_Responsive_Images" )) {
 	*/
 	class Kanedo_WP_Responsive_Images
 	{
-		
+		/**
+		 * constructor
+		 * @uses add_action
+		 * @uses add_filter
+		 * @since 1.0
+		 **/
 		function __construct()
 		{
 			add_action( 'wp_enqueue_scripts', array($this, 'register_javascript') );
@@ -48,22 +53,42 @@ if ( !class_exists( "Kanedo_WP_Responsive_Images" )) {
 			$this->register_shortcode();
 		}
 
+		/**
+		 * returns the scale factors
+		 * @return array
+		 **/
 		public function get_scale_factors(){
 			return array(2, 3);
 		}
 
+		/**
+		 * register the github update class
+		 * @see https://github.com/bfintal/Plugin-Update
+		 **/
 		public function register_github_updater(){
 			if ( is_admin() ) {
 				new BFIGitHubPluginUpdater( __FILE__, 'kanedo', "wp-responive-images" );
 			}
 		}
 
+		/**
+		 * register the javascript files
+		 * @uses wp_register_script
+		 * @uses wp_enqueue_script
+		 * @uses plugin_dir_url
+		 **/ 
 		public function register_javascript()
 		{
 			wp_register_script( 'picturefill', plugin_dir_url(__DIR__)."/assets/js/picturefill.min.js");
 			wp_enqueue_script('picturefill' );
 		}
 
+		/**
+		 * register all scaled image sizes.
+		 * only for imagesizes that doesn't match the pattern /\-\d+x/ (e.g. foo-2x)
+		 * @see self::get_scale_factors
+		 * @uses get_intermediate_image_sizes()
+		 **/ 
 		public function register_image_sizes()
 		{
 			foreach (get_intermediate_image_sizes() as $size) {
@@ -75,6 +100,13 @@ if ( !class_exists( "Kanedo_WP_Responsive_Images" )) {
 			}
 		}
 
+		/**
+		 * returns properties of image sizes. if no attribute is given all image sizes are returned
+		 * @param string $size 
+		 * @uses get_intermediate_image_sizes
+		 * @uses get_option
+		 * @return array
+		 **/
 		protected function get_image_sizes( $size = '' ) {
 
 	        global $_wp_additional_image_sizes;
@@ -117,6 +149,12 @@ if ( !class_exists( "Kanedo_WP_Responsive_Images" )) {
 	        return $sizes;
 		}
 
+		/**
+		 * register the scaled image sizes
+		 * @uses add_image_size
+		 * @param string $size
+		 * @param int $scale
+		 **/
 		protected function register_scaled_image_size( $size, $scale ){
 			$image_size = $this->get_image_sizes( $size );
 			if( array_key_exists('width', $image_size) 
@@ -126,16 +164,30 @@ if ( !class_exists( "Kanedo_WP_Responsive_Images" )) {
 			}
 		}
 
+		/**
+		 * register shortcode 'responsive_image'
+		 * @uses add_shortcode
+		 **/ 
 		public function register_shortcode()
 		{
 			add_shortcode( 'responsive_image', array($this, 'do_shortcode') );
 		}
 
+		/**
+		 * filter the send_to_editor action
+		 * @return string
+		 **/ 
 		public function send_to_editor( $html, $id, $caption, $title, $align, $url, $size, $alt )
 		{
 			return "[responsive_image id='{$id}' size='{$size}' alt='{$alt}' align='{$align}']";
 		}
 
+		/**
+		 * parse the shortcode 'responsive_image'
+		 * @param array $attr
+		 * @uses shortcode_atts
+		 * @return string
+		 **/ 
 		public function do_shortcode( $attr )
 		{
 			extract( shortcode_atts( array(
@@ -148,14 +200,30 @@ if ( !class_exists( "Kanedo_WP_Responsive_Images" )) {
 			return $this->get_img_tag( $id, $alt, $size, $align );
 		}
 
+		/**
+		 * filter the post_Thumbnail 
+		 * @param string $html
+		 * @param int $post_id
+		 * @param int $post_thumbnail_id
+		 * @param string $size
+		 * @param string $attr
+		 * @return string
+		 **/ 
 		public function filter_post_thumbnail( $html, $post_id, $post_thumbnail_id, $size, $attr ){
 			if( empty($html) ){
 				return $html;
 			}
-			$this->get_image_sizes();
 			return $this->get_img_tag( $post_thumbnail_id, '', $size);
 		}
 
+		/**
+		 * return an image tag with src set
+		 * @param int $id
+		 * @param string $alt
+		 * @param string $size
+		 * @param string $align (default '')
+		 * @return string
+		 **/ 
 		public function get_img_tag( $id, $alt, $size, $align = '')
 		{
 			$srcset = $this->get_image_src_set( $id, $size );
@@ -164,6 +232,11 @@ if ( !class_exists( "Kanedo_WP_Responsive_Images" )) {
 			return "<img src='{$srcset['1x']}' alt='{$alt}' srcset='{$srcset_string}' class='{$align} size-{$size} {$size} wp-image-{$id}' />";
 		}
 
+		/**
+		 * transform an array of hidpi images into an html srcset string
+		 * @param array $srcset
+		 * @return string
+		 **/ 
 		protected function get_src_set_string( $srcset ){
 			$srcset_string = array();
 			foreach ($srcset as $key => $value) {
@@ -172,6 +245,13 @@ if ( !class_exists( "Kanedo_WP_Responsive_Images" )) {
 			return implode(', ', $srcset_string);
 		}
 
+		/**
+		 * returns an array with all scaled images 
+		 * @param int $image the image id
+		 * @param strin $size
+		 * @uses wp_get_attachment_image_src
+		 * @return array an array [1x => '...', 2x => '...', ...]
+		 **/ 
 		protected function get_image_src_set( $image, $size )
 		{		
 			$set = array();
